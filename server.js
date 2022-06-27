2ccc4d13299f7f373e5245c08c25926f68c9f5fd
yufanme <562937707@qq.com>
2022-06-27 12:06:23 +0800

    smart-brain-full-version-back-end-333

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
-const e = require("express");
+const knex = require("knex");
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,93 +10,114 @@ app.use(cors());
 
 const port = process.env.PORT || 3000;
 
-const database = {
-  user: [
-    {
-      id: "1",
-      name: "John",
-      email: "john@gmail.com",
-      password: "hijohn",
-      entries: 0,
-      joined: new Date(),
-    },
-    {
-      id: "2",
-      name: "Sally",
-      email: "sally@gmail.com",
-      password: "hisally",
-      entries: 0,
-      joined: new Date(),
-    },
-  ],
-  login: [
-    {
-      id: "1",
-      email: "john@gmail.com",
-      hash: "$2a$10$yRb7moLGmLDqv9ra1MQASucBZu.nFQbFnea2psY883ei/OsFHN1EO",
-    },
-  ],
-};
+const smartBrainDB = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    port: 5432,
+    user: "fan",
+    password: "",
+    database: "smart-brain",
+  },
+});
 
 app.get("/", (req, res) => {
-  res.send(database.user);
+  smartBrainDB
+    .select("*")
+    .from("users")
+    .then((db) => res.json(db))
+    .catch((err) => res.status(400).json("get db error"));
 });
 
 app.post("/signin", (req, res) => {
   const { email, password } = req.body;
 
-  if (
-    email === database.user[0].email &&
-    password === database.user[0].password
-  ) {
-    res.json("signin success!");
-  } else {
-    res.status(400).json("signin failed!");
-  }
+  smartBrainDB
+    .select("email", "hash")
+    .from("login")
+    // .where({ email: email })
+    .where("email", "=", email)
+    .then((data) => {
+      if (data.length) {
+        if (bcrypt.compareSync(password, data[0].hash)) {
+          smartBrainDB
+            .select("*")
+            .from("users")
+            .where({ email: email })
+            .then((user) => res.json(user[0]))
+            .catch((err) =>
+              res.status(400).json("unable to get user in signin")
+            );
+        } else {
+          res.json("password error");
+        }
+      } else {
+        res.json("user not found");
+      }
+    })
+    .catch((err) => res.status(400).json("login in error"));
 });
 
 app.post("/register", (req, res) => {
   const { name, email, password } = req.body;
 
-  database.user.push({
-    id: "3",
-    name: name,
-    email: email,
-    password: password,
-    entries: 0,
-    joined: new Date(),
-  });
-  // send user without password to load user on the home page
-  res.json(database.user[database.user.length - 1]);
+  const hash = bcrypt.hashSync(password);
+  //use transaction
+  smartBrainDB
+    .transaction((trx) => {
+      //login table
+      smartBrainDB("login")
+        .transacting(trx)
+        .insert({
+          hash: hash,
+          email: email,
+        })
+        .returning("email")
+        .then((loginEmail) => {
+          //user table
+          trx("users")
+            .insert({
+              name: name,
+              email: loginEmail[0].email,
+              joined: new Date(),
+            })
+            .returning("*")
+            .then((user) => {
+              res.json(user[0]);
+            })
+            .catch((err) => res.status(400).json("Unable to register."));
+        })
+        //commit and rollback transaction
+        .then(trx.commit)
+        .catch(trx.rollback);
+    })
+    .catch((err) => res.status(400).json("Unable to register."));
 });
 
 app.get("/profile/:id", (req, res) => {
   const { id } = req.params;
-  let foundUser = false;
-  database.user.forEach((user) => {
-    if (user.id === id) {
-      foundUser = true;
-      return res.json(user);
-    }
-  });
-  if (!foundUser) {
-    res.status(400).json("no user with id of " + id);
-  }
+  smartBrainDB
+    .select("*")
+    .from("users")
+    .where({ id: id })
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json("User not found!");
+      }
+    })
+    .catch((err) => res.status(400).json("error getting user"));
 });
 
 app.put("/image", (req, res) => {
   const { id } = req.body;
-  let foundUser = false;
-  database.user.forEach((user) => {
-    if (user.id === id) {
-      foundUser = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  });
-  if (!foundUser) {
-    res.status(400).json("no user with id of " + id);
-  }
+  smartBrainDB("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => res.json(entries[0].entries))
+    .catch((err) => res.status(400).json("Unable to get entries."));
 });
 
 app.listen(port, () => {
@@ -104,6 +125,7 @@ app.listen(port, () => {
 });
 
 /* 
+plan of server.js
 / --> res = this is working
 /signin --> POST = success fail
 /register --> POST = userObject
